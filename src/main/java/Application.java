974d3dbf7c3b6e8a9fc6a3efb5b92df79ae0e4e8
Kanykei02 <almazbekovakany@gmail.com>
2021-04-08 17:59:02 +0600

    homework55

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,4 +1,5 @@
 import entity.Employee;
+import entity.Pizza;
 import org.hibernate.Session;
 import util.HibernateUtil;
 
@@ -15,24 +16,39 @@ public class Application {
 //        create(employee1);
 //        create(employee2);
 
-        List<Employee> employeeList = getAll();
-        System.out.println(employeeList);
+//        List<Employee> employeeList = getAll();
+//        System.out.println(employeeList);
+
+//        Pizza pizza = new Pizza(1, "Margherita", 400, 3);
+//        Pizza pizza2 = new Pizza(2, "Marinara", 325, 2);
+//        Pizza pizza3 = new Pizza(3, "Carbonara", 650, 4);
+//        Pizza pizza4 = new Pizza(4, "Crudo", 230, 2);
+//        Pizza pizza5 = new Pizza(5, "Pugliese", 198, 1);
+//
+//        create(pizza);
+//        create(pizza2);
+//        create(pizza3);
+//        create(pizza4);
+//        create(pizza5);
+
+        List<Pizza> pizzaList = getAll();
+        System.out.println(pizzaList);
     }
-    public static Integer create(Employee employee) {
+    public static Integer create(Pizza pizza) {
         Session hibernateSession = HibernateUtil.getSessionFactory().openSession();
         hibernateSession.beginTransaction();
-        hibernateSession.save(employee);
+        hibernateSession.save(pizza);
         hibernateSession.getTransaction().commit();
         hibernateSession.close();
-        System.out.println("Успешно создан " + employee.toString());
-        return employee.getId();
+        System.out.println("Успешно создан " + pizza.toString());
+        return pizza.getId();
     }
 
-    public static List<Employee> getAll() {
+    public static List<Pizza> getAll() {
         Session hibernateSession = HibernateUtil.getSessionFactory().openSession();
         @SuppressWarnings("uncheked")
-        List<Employee> employees = hibernateSession.createQuery("FROM Employee").list();
+        List<Pizza> pizzas = hibernateSession.createQuery("FROM Pizza WHERE price < 350").list();
         hibernateSession.close();
-        return employees;
+        return pizzas;
     }
 }
