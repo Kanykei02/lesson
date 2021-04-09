b1c68d7b5c809193eafa330c907c0b9e04fec2f9
Kanykei02 <almazbekovakany@gmail.com>
2021-04-09 20:38:18 +0600

    hibeer

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,6 +3,7 @@ import entity.Pizza;
 import org.hibernate.Session;
 import util.HibernateUtil;
 
+import javax.persistence.Query;
 import java.util.List;
 
 
@@ -19,20 +20,31 @@ public class Application {
 //        List<Employee> employeeList = getAll();
 //        System.out.println(employeeList);
 
-        Pizza pizza = new Pizza(1, "Margherita", 400, 3);
+//        Pizza pizza = new Pizza(1, "Margherita", 400, 3);
         Pizza pizza2 = new Pizza(2, "Marinara", 325, 2);
-        Pizza pizza3 = new Pizza(3, "Carbonara", 650, 4);
-        Pizza pizza4 = new Pizza(4, "Crudo", 230, 2);
-        Pizza pizza5 = new Pizza(5, "Pugliese", 198, 1);
+//        Pizza pizza3 = new Pizza(3, "Carbonara", 650, 4);
+//        Pizza pizza4 = new Pizza(4, "Crudo", 230, 2);
+          //Pizza pizza5 = new Pizza(5, "Pugliese", 198, 1);
+//
+//        create(pizza);
+//        create(pizza2);
+//        create(pizza3);
+//        create(pizza4);
+//        create(pizza5);
 
-        create(pizza);
-        create(pizza2);
-        create(pizza3);
-        create(pizza4);
-        create(pizza5);
+        delete(2);
+        deleteAll();
 
         List<Pizza> pizzaList = getAll();
         System.out.println(pizzaList);
+
+//        pizza5.setName("Tasty");
+//
+//        update(pizza5);
+
+
+
+
     }
     public static Integer create(Pizza pizza) {
         Session hibernateSession = HibernateUtil.getSessionFactory().openSession();
@@ -50,5 +62,45 @@ public class Application {
         List<Pizza> pizzas = hibernateSession.createQuery("FROM Pizza WHERE price < 350").list();
         hibernateSession.close();
         return pizzas;
+    }
+
+    public static void update(Pizza pizza) {
+        Session hibernateSession = HibernateUtil.getSessionFactory().openSession();
+        hibernateSession.beginTransaction();
+        Pizza pizaaFromBase = hibernateSession.load(Pizza.class, pizza.getId());
+        pizaaFromBase.setName(pizza.getName());
+        pizaaFromBase.setPrice(pizza.getPrice());
+        hibernateSession.getTransaction().commit();
+        hibernateSession.close();
+        System.out.println("Успешно изменено " + pizaaFromBase.toString());
+    }
+
+    public static Pizza findByID(Integer id) {
+        Session hibernateSession = HibernateUtil.getSessionFactory().openSession();
+        Pizza pizza = hibernateSession.load(Pizza.class, id);
+        hibernateSession.close();
+        return pizza;
+    }
+
+    public static void delete(Integer id) {
+        Session hibernateSession = HibernateUtil.getSessionFactory().openSession();
+        hibernateSession.beginTransaction();
+        Pizza pizza = findByID(id);
+        //create(pizza);
+        hibernateSession.delete(pizza);
+        //update(pizza);
+        hibernateSession.getTransaction().commit();
+        hibernateSession.close();
+        System.out.println("успешно удалено " + pizza.toString());
+    }
+
+    public static void deleteAll() {
+        Session hibernateSession = HibernateUtil.getSessionFactory().openSession();
+        hibernateSession.beginTransaction();
+        Query hibernateQuery = hibernateSession.createQuery("DELETE FROM Pizza");
+        hibernateQuery.executeUpdate();
+        hibernateSession.getTransaction().commit();
+        hibernateSession.close();
+        System.out.println("Успешно удалены все записи в Pizza.");
     }
 }
